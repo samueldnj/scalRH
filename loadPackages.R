d2b89d6348b83ef07aea39df6b30d2e68e5d1e7d
sdnjohnson <samueldnjohnson@gmail.com>
2019-02-28 12:31:02 -0800

    Fixed error for rename

---
+++ loadPackages.R
@@ -0,0 +1,36 @@
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# loadPackages.R
+# 
+# Checks if required packages are installed. If not, installs them.
+# Then loads all required packages.
+# 
+# 
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+cranPackages <- c("coda",
+                  "tidyverse",
+                  "reshape2",
+                  "ggforce",
+                  "ggplot2",
+                  "GGally",
+                  "TMB",
+                  "raster",
+                  "grid",
+                  "RColorBrewer",
+                  "HapEstXXR",
+                  "parallel",
+                  "stringr",
+                  "wesanderson",
+                  "scales",
+                  "beepr" )
+
+for( pkg in cranPackages )
+  while(!require(pkg, character.only = TRUE) )
+    install.packages( pkg )
+
+
+githubPackages <- c(ggsidekick = "seananderson/ggsidekick")
+
+for( pkgIdx in 1:length(githubPackages) )
+  while(!require(names(githubPackages)[pkgIdx], character.only = TRUE))
+    devtools::install_github(githubPackages[pkgIdx])
