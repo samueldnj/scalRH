e9fd3e8a39dc2b0df7009f9e746440f65e72a64c
sdnjohnson <samueldnjohnson@gmail.com>
2019-03-07 12:58:17 -0800

    Added ref points

---
+++ refPts.R
@@ -0,0 +1,324 @@
+# --------------------------------------------------------------------------
+# refPts.R
+# 
+# Takes parameters from a report object and calculates
+# reference points and curves under an age structured/YPR formulation. 
+# Called from within runHierSCAL() after runnings an AM.
+# 
+# 
+# Author: Samuel Johnson
+# Date: March 7, 2019
+# 
+# --------------------------------------------------------------------------
+
+# calcRefPts()
+# Calculates biological reference points based on given biological parameters 
+# assuming a delay difference biological model.
+# inputs:   obj = list of biological parameters
+# ouputs:   refPts = list() of reference points
+calcRefPts <- function( obj )
+{
+  # Pull number of species
+  nS          <- obj$nS
+  nP          <- obj$nP
+
+  # Calculate reference curves
+  refCurves <- .calcRefCurves( obj )
+
+  # First, let's just do Fmsy reference points
+  FmsyRefPts          <- vector( mode = "list", length = nS )
+  names( FmsyRefPts ) <- obj$speciesNames
+  for( sIdx in 1:nS )
+  {
+    FmsyRefPts <- .getFmsy_sp(  obj = obj, 
+                                refCurves = refCurves )
+  }
+  
+  obj$refPts <- list()
+  obj$refPts$refCurves    <- refCurves
+  obj$refPts$FmsyRefPts   <- FmsyRefPts
+  obj
+}
+
+# .calcRefCurves()
+# Calculates equilibrium curves of equilbrium biomass, numbers,
+# yield and recruitment as a function of input fishing mortality rates
+# inputs:   obj = list of biological parameters
+# ouputs:   refCurves = list() of reference curves (vectors)
+.calcRefCurves <- function( obj, nFs = 1000 )
+{
+  # First, compute max F (tolerance of 1e-5)
+  maxF <- max( 3*obj$M_sp )
+
+  # We're going to need to fill each species' ref curves,
+  # so labeling and dimensions are needed
+  nS          <- obj$nS
+  nP          <- obj$nP
+  specNames   <- dimnames(obj$M_sp)[[1]]
+  stockNames  <- dimnames(obj$M_sp)[[2]]
+
+
+  f <- seq( from = 0.0, to = maxF, length = nFs )
+
+  # Create matrices to hold Recruitment reference curve, name rows and copy
+  # for each of wbar, Beq, Neq and Yeq
+  Req_spf      <- array( NA,  dim = c(nS, nP, nFs),
+                              dimnames = list(  species = specNames,
+                                                stock = stockNames,
+                                                F = f ) )
+  Beq_spf       <- Req_spf
+  Yeq_spf       <- Req_spf
+  ypr_spf       <- Req_spf
+
+  # Loop and fill
+  for( i in 1:length(f) )
+  {
+    tmp         <- .calcEquil( f = f[i], obj = obj )
+    Req_spf[,,i]    <- tmp$Req
+    Beq_spf[,,i]    <- tmp$Beq
+    Yeq_spf[,,i]    <- tmp$Yeq
+    ypr_spf[,,i]    <- tmp$ypr
+  }
+
+  refCurves <- list()
+    refCurves$F         <- f
+    refCurves$ypr_spf   <- ypr_spf
+    refCurves$Req_spf   <- Req_spf
+    refCurves$Beq_spf   <- Beq_spf
+    refCurves$Yeq_spf   <- Yeq_spf
+
+  return( refCurves )
+}
+
+# .calcEquil()
+# Calculates equilibrium Biomass, Yield and Recruitment 
+# with respect to given fishing mortality rates F
+# inputs:   f = input fishing mortality rate
+#           obj = list of biological parameters
+# ouputs:   equil = list() of equilibrium biomass, yield and recruitment
+.calcEquil <- function( f = 0, obj )
+{
+  nS  <- obj$nS
+  nP  <- obj$nP
+  A_s <- obj$A_s
+
+  # Recover recruitment pars
+  h_sp          <- obj$h_sp
+  rec.a_sp      <- obj$reca_sp
+  rec.b_sp      <- obj$recb_sp
+
+  # To get things moving, let's fix
+  # steepness at .78
+  h_sp      <- matrix(.78, nrow = nS, ncol = nP)
+  B0_sp     <- obj$B0_sp
+  R0_sp     <- obj$R0_sp
+
+  # Beverton-Holt a/b parameters
+  rec.a_sp <- 4.*h_sp*R0_sp/(B0_sp*(1.-h_sp))
+  rec.b_sp <- (5.*h_sp-1.)/(B0_sp*(1.-h_sp))
+
+
+  # Now calculate eqbm recruitment at given f value
+  tmp <- .calcPerRecruit( f = f, obj = obj )
+  yprList <- tmp$yprList
+
+  recruits_sp <- ( rec.a_sp * yprList$ssbpr_sp - 1) / (rec.b_sp * yprList$ssbpr_sp)
+  
+
+  equil <- list()
+    equil$Req_sp     <- recruits_sp
+    equil$Beq_sp     <- recruits_sp * yprList$ssbpr_sp
+    equil$Yeq_sp     <- recruits_sp * yprList$ypr_sp
+    equil$ypr_sp     <- yprList$ypr_sp
+    equil$ssbpr_sp   <- yprList$ssbpr_sp
+
+  return(equil)
+}
+
+# .calcPerRecruit
+# Purpose:     Calculate all equilibrium per-recruit quantities of interest for an
+#              input fishing mortality.
+# Parameters:  f=scalar input fishing mortality rate; obj=list of all operating
+#              model parameters.
+# Returns:     a list with equilibrium quantities - (i) spawning stock biomass-per-recruit
+#              and (ii) yield-per-recruit (ypr)
+# Source:      S.P. Cox, modified for hierSCAL by SDNJ
+.calcPerRecruit <- function( f, obj )
+{
+
+  # Compute eqbm spawning biomass per recruit for
+  # given f and species/stock pars
+  nS    <- obj$nS
+  nP    <- obj$nP
+  A_s   <- obj$A_s
+  nA    <- obj$nA
+  nL    <- obj$nL
+  M_sp  <- obj$M_sp
+
+  # Life history schedules
+  matAge_asp      <- obj$matAge_asp
+  lenAge_asp      <- obj$lenAge_asp
+  wtAge_asp       <- obj$meanWtAge_asp
+  probLenAge_lasp <- obj$probLenAge_lasp
+
+  # Selectivity - this is mean over each fleet's 
+  # time period, not time-varying
+  # Might be useful to take the group mean for the comm fleet...
+  xSel50_sp   <- obj$xSel50_spf[,,2]
+  xSel95_sp   <- obj$xSel95_spf[,,2]
+  xSelStep_sp <- obj$xSelStep_spf[,,2]
+
+  # Harcode for comm.mod for now,
+  # and length based selectivity
+  selLen_lsp <- array(NA, dim = c(nL,nS,nP) )
+  selAge_asp <- array(NA, dim = c(nA,nS,nP) )
+
+  # Loop over species and stocks, calculate
+  # selLen so we can get selAge
+  for( s in 1:nS )
+    for(p in 1:nP )
+    {
+      selLen_lsp[,s,p] <- (1 + exp(-(1:nL - xSel50_sp[s,p])/xSelStep_sp[s,p]/log(19)))^(-1)
+      for( a in 1:nA )
+      {
+        selAge_asp[a,s,p] <- sum(probLenAge_lasp[,a,s,p] * selLen_lsp[,s,p])
+      }
+      selAge_asp[,s,p] <- selAge_asp[,s,p] / max(selAge_asp[,s,p])
+    }
+
+  # Compute Z_asp
+  Z_asp <- array( NA, dim = c(nA,nS,nP))
+  Surv_asp <- array( NA, dim = c(nA,nS,nP))
+  Surv_asp[1,,] <- 1
+  for( s in 1:nS )
+    for( p in 1:nP )
+    {
+      Z_asp[1:A_s[s],s,p] <- M_sp[s,p]
+      for( a in 1:A_s[s])
+      {
+        Z_asp[a,s,p] <- Z_asp[a,s,p] + selAge_asp[a,s,p] * f
+        if( a > 1 )
+          Surv_asp[a,s,p] <- Surv_asp[a-1,s,p] * exp( -Z_asp[a-1,s,p])
+        if( a == A_s[s])
+          Surv_asp[a,s,p] <- Surv_asp[a,s,p] / (1 - exp(-Z_asp[a,s,p]))
+      }
+    }
+
+  # Calculate yield-per-recruit
+  C_asp     <- Surv_asp * wtAge_asp * selAge_asp * f * (1 - exp(-Z_asp))/Z_asp
+  # Replace NAs with 0 (unmodeled ages)
+  Z_asp[is.na(Z_asp)] <- 0
+  Surv_asp[is.na(Surv_asp)] <- 0
+  C_asp[is.na(C_asp)] <- 0
+  
+  # Compute YPR
+  ypr_sp    <- apply( X = C_asp, FUN = sum, MARGIN = c(2,3),na.rm = T)
+
+  # spawning biomass per recruit
+  ssbpr_asp  <- Surv_asp * wtAge_asp * matAge_asp
+  ssbpr_sp   <- apply( X = ssbpr_asp, FUN = sum, MARGIN = c(2,3), na.rm = T )
+
+  # compile output list
+  yprList <- list(  ssbpr_sp  = ssbpr_sp,
+                    ypr_sp    = ypr_sp )
+
+  obj$yprList <- yprList
+
+  return(obj)
+}
+
+# Calculates recruitment parameters, and equilibrium unfished
+# numbers and recruitment.
+.calcRecPars <- function( obj )
+{
+  # Calculate eqbm parameters
+  # Survival
+
+    
+  # Beverton-Holt a parameters
+  rec.a <- 4.*obj$rSteepness*R0/(obj$B0*(1.-obj$rSteepness))
+  rec.b <- (5.*obj$rSteepness-1.)/(obj$B0*(1.-obj$rSteepness))
+
+  # Now return everything in a list
+  recList <- list(  S0 = S0,
+                    wbar0 = wbar0,
+                    N0 = N0,
+                    R0 = R0,
+                    rec.a = rec.a,
+                    rec.b = rec.b  )
+
+  return(recList)
+}
+
+# .getFmsy     ()
+# Purpose:     fit a spline function to f vs yield, then use a root finder to get Fmsy. 
+# Parameters:  obj=list of all operating model parameters, schedules, equilibrium functions
+# Returns:     a list with all equilibrium quantities for Fmsy
+# Source:      S.P. Cox, modified for hierSCAL by SDNJ
+.getFmsy_sp <- function( obj, refCurves )
+{
+  Fseq  <- refCurves$F
+  nS    <- obj$nS
+  nP    <- obj$nP
+
+  .getFmsy <- function( yieldCurve, F = Fseq )
+  {
+    minF <- 0
+    maxF <- max(F)
+
+    # Now check if yieldCurve goes negative anywhere
+    if( any(yieldCurve < 0) )
+    {
+      minF <- F[min(which(yieldCurve >= 0))]
+      maxF <- F[max(which(yieldCurve >= 0))]
+    }
+
+    fySplineFun <- splinefun( x=F, y=yieldCurve )  
+
+    # Find stat point for Fmsy
+    Fmsy <- try( uniroot( f = fySplineFun, interval = c(minF, maxF),
+                          deriv = 1 )$root )
+    if(class(Fmsy) == "try-error")
+    {
+      browser(cat("uniroot failed\n\n"))
+      Fmsy <- 0
+    }
+
+    Fmsy <- min(Fmsy, maxF)
+
+    return(Fmsy)
+  }
+
+  # calculate Fmsy for each species/stock
+  Fmsy_sp <-  apply(  X = refCurves$Yeq_spf, FUN = .getFmsy,
+                      MARGIN = c(1,2) )
+  # Now create a matrix to hold the species/stock values
+  # on each curve
+  spMat       <- matrix(0, nrow = nS, ncol = nP)
+  dimnames(spMat) <- dimnames(Fmsy_sp)
+
+  FmsyRefPts  <- list(  yprFmsy_sp = spMat,
+                        YeqFmsy_sp = spMat,
+                        BeqFmsy_sp = spMat,
+                        ReqFmsy_sp = spMat )
+
+  
+  # Calculate ref points
+  FmsyRefPts$Fmsy_sp    <- Fmsy_sp
+
+  # Loop and get each stock/species ref pt
+  for( s in 1:nS )
+    for( p in 1:nP )
+    {
+      tmp <- .calcEquil( f = Fmsy_sp[s,p], obj = obj )
+      FmsyRefPts$yprFmsy_sp[s,p] <- tmp$ypr_sp[s,p]
+      FmsyRefPts$YeqFmsy_sp[s,p] <- tmp$Yeq_sp[s,p]
+      FmsyRefPts$BeqFmsy_sp[s,p] <- tmp$Beq_sp[s,p]
+      FmsyRefPts$NeqFmsy_sp[s,p] <- tmp$Neq_sp[s,p]
+      FmsyRefPts$ReqFmsy_sp[s,p] <- tmp$Req_sp[s,p]
+    }
+
+  FmsyRefPts
+}     # END function .getFmsy
+
+
